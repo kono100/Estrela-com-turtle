de2b3ce42add12d9052d1de8a5191ddb3aea93b9
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-04-01 18:33:59 -0300

    Add files via upload

---
+++ Estrela com turtle.py
@@ -0,0 +1,14 @@
+
+
+import turtle
+import time
+
+turtle.color('yellow')
+turtle.bgcolor('black')
+
+for i in range(5):
+    turtle.width(13)
+    turtle.forward(300)
+    turtle.right(144)
+
+time.sleep(10)
